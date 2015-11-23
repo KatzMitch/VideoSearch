b94d3d3328d8c64fba025f511a267d708502aeaf
KatzMitch <mitchellmkatz@gmail.com>
2015-11-22 19:01:33 -0500

    Push for posterity

--- src/chunk_compare.py
+++ src/chunk_compare.py
@@ -3,7 +3,7 @@ from __future__ import division
 import multiprocessing as mp
 import numpy
 from framediff import frame_rmse
-from time_convert import timestamp_to_seconds
+from time_convert import timestamp_to_seconds, seconds_to_timestamp
 from moviepy.video.io.ffmpeg_reader import FFMPEG_VideoReader
 from moviepy.video.io.ffmpeg_reader import ffmpeg_parse_infos
 from math import floor
@@ -11,19 +11,49 @@ from PIL import Image
 import ctypes
 
 FPS = 30
-run_avg = 0.0
 
-def comparechunk(orig_vid_name, comp_vid_name, comp_vid_start, comp_vid_end):
-	return
+mutex = mp.Semaphore(1)
+ovid_name = ""
+cvid_name = ""
+
+def map_func(startpoint):
+	startpoint_compare(ovid_name, cvid_name, startpoint)
+
+def comparechunk(orig_vid_name, comp_vid_name, comp_vid_start, comp_vid_end, thresh):
 	# for frame in comp_vid
-		# get png_rmse(0O, xC)
-		# if above threshold
-			# call startpoint_compare
+	comp_vid = FFMPEG_VideoReader(comp_vid_name)
+	orig_vid = FFMPEG_VideoReader(orig_vid_name)
+	frameO = orig_vid.get_frame(0)
+
+	comp_start = int(floor(timestamp_to_seconds(comp_vid_start) * comp_vid.fps))
+	comp_end = int(floor(timestamp_to_seconds(comp_vid_end) * comp_vid.fps))
+
+	frameC = comp_vid.get_frame(comp_start)
+	below_thresh = []
+	for i in range(comp_start, comp_end):
+		score = frame_rmse(frameO, frameC)
+		print "Score for: ", i, " is: ", score
+		if frame_rmse(frameO, frameC) < thresh:
+			below_thresh.append(i)
+		frameC = comp_vid.read_frame()
+
+	pool = mp.Pool(10)
+	mutex.acquire()
+	ovid_name = orig_vid_name
+	cvid_name = comp_vid_name
+	ret = pool.map(map_func, below_thresh)
+	mutex.release()
+	pool.close()
+	pool.join()
+
+	print ret
+
+def score_callback(score):
+	print "SCORE: ", score
 
 def startpoint_compare(orig_vid_name, comp_vid_name, comp_vid_start):
 	comp_vid = FFMPEG_VideoReader(comp_vid_name)
 	orig_vid = FFMPEG_VideoReader(orig_vid_name)
-	comp_vid_start = timestamp_to_seconds(comp_vid_start)
 
 	## Hopefully this invariant will be tested sooner
 	if comp_vid.fps != orig_vid.fps:
@@ -31,23 +61,16 @@ def startpoint_compare(orig_vid_name, comp_vid_name, comp_vid_start):
 		exit(1)
 
 	fileinfo = ffmpeg_parse_infos(orig_vid_name)
-	print "length", length = int(floor(fileinfo['duration'] * fileinfo['video_fps']))
-    print "frames", print orig_vid.nframes
+	length = int(floor(fileinfo['duration'] * fileinfo['video_fps']))
 
 	frameC = comp_vid.get_frame(comp_vid_start * fileinfo['video_fps'])
 	comp_vid.read_frame() # Sync up each video. NOTE: temporary solution; there
 						  # should be a "seek" method we can use
 	frameO = orig_vid.read_frame()
-	for i in range(comp_vid_start, length + comp_vid_start):
-		calc_rmse(frameC, frameO)
+	run_avg = 0
+	for i in range(0, length):
+		run_avg += frame_rmse(frameC, frameO)
 		frameO = orig_vid.read_frame()
 		frameC = comp_vid.read_frame()
 
-	print "SCORE: ", run_avg / length
 	return run_avg / length
-
-def calc_rmse(frameC, frameO):
-	global run_avg
-	score = frame_rmse(frameC, frameO)
-	run_avg += score
-	print score
--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
 import chunk_compare
 
 if __name__ == '__main__':
-	chunk_compare.startpoint_compare("../TestFiles/Kimmy.mp4", "../TestFiles/Kimmy2.mp4", "0:0:0")
+	chunk_compare.comparechunk("../TestFiles/Sail1.mp4", "../TestFiles/Sail2.mp4", "0:0:0", "0:0:30", 60)
